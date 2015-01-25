a084387eae02142a0e8386c05c9606cf721cb9ec
thinksea <thinksea@163.com>
2015-01-25 20:17:37 +0800

    update 0.6.0 支持选择分页尺寸

--- README.md
+++ README.md
@@ -5,14 +5,188 @@
 
 ---
 
-#### version：0.5
+###### version：0.6.0
+
++ 1、支持选择分页尺寸。
+
+###### version：0.5
 
 + 1、支持布局方案设置，控制元素排列顺序，显示/隐藏元素。
 
-#### version：0.4.0
+###### version：0.4.0
 
 + 1、支持通过程序代码设置参数。
 + 2、支持通过HTML元素属性设置参数。
 + 3、支持多个控件联动效果
 + 4、支持输入页码跳转
 + 5、支持元素隐藏/显示控制
+
+---
+
+# 需要在 HTML 中引入的资源
+
+```html
+<script type="text/javascript" charset="utf-8" src="Scripts/jquery-1.11.0.min.js"></script>
+<link type="text/css" href="Scripts/bootstrap-3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+<script type="text/javascript" charset="utf-8" src="Scripts/bootstrap-3.3.1/js/bootstrap.min.js"></script>
+<link type="text/css" href="bootstrap-pagination.min.css" rel="stylesheet" />
+<script type="text/javascript" charset="utf-8" src="bootstrap-pagination.min.js"></script>
+```
+
+---
+
+# 最简短代码示例
+
+###### html代码
+
+```html
+<nav>
+    <ul id="demo1" class="pagination">
+    </ul>
+</nav>
+```
+
+###### javascript代码
+
+```javascript
+<script type="text/javascript">
+    $(function () {
+        var demo1 = BootstrapPagination($(".demo1"), {
+            //记录总数。
+            total: 101,
+            //当前页索引编号。从其开始（从0开始）的整数。
+            pageIndex: 2,
+            //当分页更改后引发此事件。
+            pageChanged: function (pageIndex, pageSize) {
+                alert("page changed. pageIndex:" + pageIndex + ",pageSize:" + pageSize)
+            },
+        });
+    });
+</script>
+```
+
+# 通过程序代码设置参数
+
+###### html代码
+
+```html
+<nav>
+    <ul id="demo2" class="pagination">
+    </ul>
+</nav>
+```
+
+###### javascript代码
+
+```javascript
+<script type="text/javascript">
+    $(function () {
+        var demo2 = BootstrapPagination($("#demo1"), {
+            layoutScheme: "lefttext,firstpage,prevgrouppage,prevpage,pagenumber,nextpage,nextgrouppage,lastpage,pagesizelist,pageinput,righttext",
+            //记录总数。
+            total: 101,
+            //分页尺寸。指示每页最多显示的记录数量。
+            pageSize: 20,
+            //当前页索引编号。从其开始（从0开始）的整数。
+            pageIndex: 2,
+            //指示分页导航栏中最多显示的页索引数量。
+            pageGroupSize: 5,
+            //位于导航条左侧的输出信息格式化字符串
+            leftFormateString: "本页{count}条记录/共{total}条记录",
+            //位于导航条右侧的输出信息格式化字符串
+            rightFormateString: "第{pageNumber}页/共{totalPages}页",
+            //页码文本格式化字符串。
+            pageNumberFormateString: "{pageNumber}",
+            //上一页导航按钮文本。
+            prevPageText: "上一页",
+            //下一页导航按钮文本。
+            nextPageText: "下一页",
+            //上一组分页导航按钮文本。
+            prevGroupPageText: "上一组",
+            //下一组分页导航按钮文本。
+            nextGroupPageText: "下一组",
+            //首页导航按钮文本。
+            firstPageText: "首页",
+            //尾页导航按钮文本。
+            lastPageText: "尾页",
+            //设置页码输入框中显示的提示文本。
+            pageInputPlaceholder: "GO",
+            //接受用户输入内容的延迟时间。单位：毫秒
+            pageInputTimeout: 800,
+            //分页尺寸列表。
+            pageSizeList: [5, 10, 20, 50, 100, 200],
+            //当分页更改后引发此事件。
+            pageChanged: function (pageIndex, pageSize) {
+                alert("page changed. pageIndex:" + pageIndex + ",pageSize:" + pageSize)
+            },
+        });
+    });
+</script>
+```
+
+# 通过HTML元素属性设置参数
+
+###### html代码
+
+```html
+<nav>
+    <ul class="pagination demo3" data-total="101" data-pageindex="1" data-pagesize="20" data-pagegroupsize="5"
+        data-leftformatestring="本页{count}条记录/共{total}条记录"
+        data-rightformatestring="第{pageNumber}页/共{totalPages}页"
+        data-pagenumberformatestring="{pageNumber}"
+        data-prevpagetext="上一页" data-nextpagetext="下一页"
+        data-prevgrouppagetext="上一组" data-nextgrouppagetext="下一组"
+        data-firstpagetext="首页" data-lastpagetext="尾页"
+        data-pageinput-placeholder="GO" data-pageinput-timeout="800"
+        data-pagesizelist="[5, 10, 20, 50, 100, 200]"
+        data-pagechanged='function (pageIndex, pageSize) {alert("page changed. pageIndex:" + pageIndex + ",pageSize:" + pageSize);};'
+        data-layoutscheme="lefttext,firstpage,prevgrouppage,prevpage,pagenumber,nextpage,nextgrouppage,lastpage,pagesizelist,pageinput,righttext">
+    </ul>
+</nav>
+```
+
+###### javascript代码
+
+```javascript
+<script type="text/javascript">
+    $(function () {
+        var demo3 = BootstrapPagination($(".demo3"));
+    });
+</script>
+```
+
+# 多个控件具有联动效果
+
+###### html代码
+
+```html
+<nav>
+    <ul class="pagination demo4" data-total="101" data-pageindex="1" data-pagesize="20" data-pagegroupsize="5"
+        data-leftformatestring="本页{count}条记录/共{total}条记录"
+        data-rightformatestring="第{pageNumber}页/共{totalPages}页"
+        data-pagenumberformatestring="{pageNumber}"
+        data-prevpagetext="上一页" data-nextpagetext="下一页"
+        data-prevgrouppagetext="上一组" data-nextgrouppagetext="下一组"
+        data-firstpagetext="首页" data-lastpagetext="尾页"
+        data-pageinput-placeholder="GO" data-pageinput-timeout="800"
+        data-pagesizelist="[5, 10, 20, 50, 100, 200]"
+        data-pagechanged='function (pageIndex, pageSize) {alert("page changed. pageIndex:" + pageIndex + ",pageSize:" + pageSize);};'
+        data-layoutscheme="lefttext,firstpage,prevgrouppage,prevpage,pagenumber,nextpage,nextgrouppage,lastpage,pagesizelist,pageinput,righttext">
+    </ul>
+</nav>
+一般情况这里显示数据列表
+<nav>
+    <ul class="pagination demo4">
+    </ul>
+</nav>
+```
+
+###### javascript代码
+
+```javascript
+<script type="text/javascript">
+    $(function () {
+        var demo4 = BootstrapPagination($(".demo4"));
+    });
+</script>
+```
--- bootstrap-pagination.js
+++ bootstrap-pagination.js
@@ -2,15 +2,15 @@
 
 /*
 定义一个 javascript 分页插件，基于 bootstrap 样式。
-version：0.5.1
-last change：2015-1-24
+version：0.6.0
+last change：2015-1-25
 */
 var BootstrapPagination = function (obj, option) {
     var innerBootstrapPagination = function (obj, option) {
         this.options = {
             //记录总数。
             total: 0,
-            //指示每页最多显示的记录数量。
+            //分页尺寸。指示每页最多显示的记录数量。
             pageSize: 20,
             //当前页索引编号。从其开始（从0开始）的整数。
             pageIndex: 0,
@@ -19,7 +19,7 @@ var BootstrapPagination = function (obj, option) {
             //位于导航条左侧的输出信息格式化字符串
             leftFormateString: "本页{count}条记录/共{total}条记录",
             //位于导航条右侧的输出信息格式化字符串
-            rightFormateString: "第{pageNumber}页/共{pageCount}页",
+            rightFormateString: "第{pageNumber}页/共{totalPages}页",
             //页码文本格式化字符串。
             pageNumberFormateString: "{pageNumber}",
             //上一页导航按钮文本。
@@ -35,13 +35,15 @@ var BootstrapPagination = function (obj, option) {
             //尾页导航按钮文本。
             lastPageText: "尾页",
             //设置页码输入框中显示的提示文本。
-            gotoInputPlaceholder: "GO",
+            pageInputPlaceholder: "GO",
             //接受用户输入内容的延迟时间。单位：毫秒
-            enterTimeout: 800,
+            pageInputTimeout: 800,
+            //分页尺寸列表。
+            pageSizeList: [5, 10, 20, 50, 100, 200],
             //当分页更改后引发此事件。
             pageChanged: function (pageIndex, pageSize) { },
             //布局方案。指示按照什么样的排列顺序显示哪些元素。
-            layoutScheme: "lefttext,firstpage,prevgrouppage,prevpage,pagenumber,nextpage,nextgrouppage,lastpage,gotoinput,righttext",
+            layoutScheme: "lefttext,firstpage,prevgrouppage,prevpage,pagenumber,nextpage,nextgrouppage,lastpage,pagesizelist,pageinput,righttext",
         };
 
         // 获取或设置分页索引。
@@ -50,23 +52,39 @@ var BootstrapPagination = function (obj, option) {
                 return this.options.pageIndex;
             }
             else {
-                $.extend(true, this.options, {
-                    pageIndex: newPageIndex,
-                });
+                this.options.pageIndex = newPageIndex;
+                //$.extend(true, this.options, {
+                //    pageIndex: newPageIndex,
+                //});
+                this.fixPageIndex();
                 this.render();
                 if (this.options.pageChanged) {
-                    this.options.pageChanged(newPageIndex, this.options.pageSize);
+                    this.options.pageChanged(this.options.pageIndex, this.options.pageSize);
+                }
+            }
+        }
+
+        // 获取或设置分页尺寸。
+        this.pageSize = function (newPageSize) {
+            if (newPageSize === undefined) {
+                return this.options.pageSize;
+            }
+            else {
+                this.options.pageSize = newPageSize;
+                this.render();
+                if (this.options.pageChanged) {
+                    this.options.pageChanged(this.options.pageIndex, this.options.pageSize);
                 }
             }
         }
 
         // 获取分页总数。
-        this.pageCount = function () {
+        this.totalPages = function () {
             return Math.floor((this.options.total + this.options.pageSize - 1) / this.options.pageSize);
         }
 
         // 获取当前页实际显示的记录数量。
-        this.currentRecordsCount = function () {
+        this.currentCount = function () {
             var rCount = this.options.total - this.options.pageSize * this.options.pageIndex;
             if (rCount > this.options.pageSize) {
                 return this.options.pageSize;
@@ -102,21 +120,58 @@ var BootstrapPagination = function (obj, option) {
 
         // 执行格式化字符串。
         this.doFormateString = function (formateString) {
-            return formateString.replace("{count}", this.currentRecordsCount())
+            return formateString.replace("{count}", this.currentCount())
                     .replace("{total}", this.options.total)
                     .replace("{pageNumber}", this.options.total > 0 ? this.options.pageIndex + 1 : 0)
-                    .replace("{pageCount}", this.pageCount());
+                    .replace("{totalPages}", this.totalPages());
+        }
+
+        // 创建分页尺寸列表控件。
+        this.createPageSizeList = function (align) {
+            var li = $('<li></li>');
+            var el1 = $('<div class="input-group dropup" style="float: left;"></div>');
+            var el2 = $('<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="true"><span class="pagesize">' + this.options.pageSize + '</span><span class="caret"></span></button>');
+            switch (align) {
+                case 1:
+                    el2.removeClass("pagesize-fix-center").removeClass("pagesize-fix-right").addClass("pagesize-fix-left");
+                    break;
+                case 2:
+                    el2.removeClass("pagesize-fix-left").removeClass("pagesize-fix-right").addClass("pagesize-fix-center");
+                    break;
+                case 3:
+                    el2.removeClass("pagesize-fix-left").removeClass("pagesize-fix-center").addClass("pagesize-fix-right");
+                    break;
+            }
+            var el3 = $('<ul class="dropdown-menu" role="menu"></ul>');
+            li.append(el1);
+            el1.append(el2);
+            el1.append(el3);
+            for (var i = 0; i < this.options.pageSizeList.length; i++) {
+                var tmp = this.options.pageSizeList[i];
+                var liItem = $('<li role="presentation"></li>');
+                if (tmp == this.options.pageSize) {
+                    liItem.attr("class", "active");
+                }
+                var a = $('<a role="menuitem" tabindex="-1" href="javascript:;">' + tmp + '</a>');
+                a.off('click').on('click', $.proxy(this.pageSize, this, tmp));
+                liItem.append(a);
+                el3.append(liItem);
+            }
+            return li;
         }
 
         // 创建页码输入框。
-        this.createGoto = function () {
-            var li = $('<li class="gotoInput"></li>');
-            var inputgroup = $('<div class="input-group"></div>');
+        this.createPageInput = function () {
+            var li = $('<li></li>');
+            var inputgroup = $('<div class="input-group page-input"></div>');
             li.append(inputgroup);
-            var input = $('<input type="text" class="form-control" placeholder="' + this.options.gotoInputPlaceholder + '" />');
+            var input = $('<input type="text" class="form-control" />');
+            if (this.options.pageInputPlaceholder) {
+                input.attr("placeholder", this.options.pageInputPlaceholder);
+            }
             inputgroup.append(input);
 
-            input.off('keyup').on('keyup', $.proxy(this.gotoEvents, this));
+            input.off('keyup').on('keyup', $.proxy(this.pageInputEvents, this));
 
             //<input type="text" class="form-control" placeholder="GO" />
             //    <div class="input-group-btn">
@@ -127,7 +182,7 @@ var BootstrapPagination = function (obj, option) {
 
         this.timeoutId = 0;
         // 处理输入页码事件。
-        this.gotoEvents = function (event) {
+        this.pageInputEvents = function (event) {
             var ctl = $(event.target);
             var sNum = ctl.val();
             var reg = /^\d+$/gi;
@@ -138,17 +193,30 @@ var BootstrapPagination = function (obj, option) {
             }
             ctl.parent().removeClass("has-error");
             var pageNum = parseInt(sNum) - 1;
-            if (pageNum > (this.pageCount() - 1)) {
-                pageNum = this.pageCount() - 1;
-            }
-            if (pageNum < 0) {
-                pageNum = 0;
-            }
+            //if (pageNum > (this.totalPages() - 1)) {
+            //    pageNum = this.totalPages() - 1;
+            //}
+            //if (pageNum < 0) {
+            //    pageNum = 0;
+            //}
             var that = this;
             clearTimeout(this.timeoutId);
             this.timeoutId = setTimeout(function () {
                 that.pageIndex(pageNum);
-            }, this.options.enterTimeout);
+            }, this.options.pageInputTimeout);
+        }
+
+        // 调整页码索引。
+        this.fixPageIndex = function () {
+            {
+                var tp = this.totalPages();
+                if (this.options.pageIndex > tp - 1) {
+                    this.options.pageIndex = tp - 1;
+                }
+                if (this.options.pageIndex < 0) {
+                    this.options.pageIndex = 0;
+                }
+            }
         }
 
         // 呈现控件。
@@ -208,13 +276,13 @@ var BootstrapPagination = function (obj, option) {
                         //#region 页索引
                         if (this.options.pageNumberFormateString) {
                             var pageNum = this.options.pageIndex - Math.floor((this.options.pageGroupSize - 1) / 2); //分页页码。
-                            if (pageNum + this.options.pageGroupSize > this.pageCount() - 1) {
-                                pageNum = this.pageCount() - this.options.pageGroupSize;
+                            if (pageNum + this.options.pageGroupSize > this.totalPages() - 1) {
+                                pageNum = this.totalPages() - this.options.pageGroupSize;
                             }
                             if (pageNum < 0) {
                                 pageNum = 0;
                             }
-                            for (var i = 0; i < this.options.pageGroupSize && pageNum < this.pageCount() ; i++) {
+                            for (var i = 0; i < this.options.pageGroupSize && pageNum < this.totalPages() ; i++) {
                                 lis[lis.length] = this.createPageButton(pageNum + 1, pageNum);
                                 pageNum++;
                             }
@@ -224,7 +292,7 @@ var BootstrapPagination = function (obj, option) {
                     case "nextpage":
                         //#region 下一页按钮
                         if (this.options.nextPageText) {
-                            if (this.options.pageIndex < this.pageCount() - 1) {
+                            if (this.options.pageIndex < this.totalPages() - 1) {
                                 var pageNum = this.options.pageIndex + 1;
                                 lis[lis.length] = this.createPageButton(this.options.nextPageText, pageNum);
                             }
@@ -237,8 +305,8 @@ var BootstrapPagination = function (obj, option) {
                     case "nextgrouppage":
                         //#region 下一组分页按钮
                         if (this.options.nextGroupPageText) {
-                            if (this.options.pageIndex < this.pageCount() - 1) {
-                                var pageNum = (this.options.pageIndex + this.options.pageGroupSize > this.pageCount() - 1) ? this.pageCount() - 1 : this.options.pageIndex + this.options.pageGroupSize;
+                            if (this.options.pageIndex < this.totalPages() - 1) {
+                                var pageNum = (this.options.pageIndex + this.options.pageGroupSize > this.totalPages() - 1) ? this.totalPages() - 1 : this.options.pageIndex + this.options.pageGroupSize;
                                 lis[lis.length] = this.createPageButton(this.options.nextGroupPageText, pageNum);
                             }
                             else {
@@ -250,8 +318,8 @@ var BootstrapPagination = function (obj, option) {
                     case "lastpage":
                         //#region 尾页按钮
                         if (this.options.lastPageText) {
-                            if (this.options.pageIndex < this.pageCount() - 1) {
-                                var pageNum = this.pageCount() - 1;
+                            if (this.options.pageIndex < this.totalPages() - 1) {
+                                var pageNum = this.totalPages() - 1;
                                 lis[lis.length] = this.createPageButton(this.options.lastPageText, pageNum);
                             }
                             else {
@@ -260,9 +328,28 @@ var BootstrapPagination = function (obj, option) {
                         }
                         //#endregion
                         break;
-                    case "gotoinput":
+                    case "pageinput":
                         //#region 处理页码输入框
-                        lis[lis.length] = this.createGoto();
+                        lis[lis.length] = this.createPageInput();
+                        //#endregion
+                        break;
+                    case "pagesizelist":
+                        //#region 处理分页尺寸列表控件
+                        if (this.options.pageSizeList) {
+                            var align = 0;
+                            if (layoutItems.length > 1) {
+                                if (i_layout > 0 && i_layout < layoutItems.length - 1) { //在中间
+                                    align = 2;
+                                }
+                                else if (i_layout == 0) { //在左侧
+                                    align = 1;
+                                }
+                                else if (i_layout == layoutItems.length - 1) { //在右侧
+                                    align = 3;
+                                }
+                            }
+                            lis[lis.length] = this.createPageSizeList(align);
+                        }
                         //#endregion
                         break;
                     case "righttext":
@@ -282,14 +369,16 @@ var BootstrapPagination = function (obj, option) {
         this.init = function () {
             //obj = $(obj);
             //#region 根据 HTML 标签上的 data- 属性初始化参数。
+            if (obj.data("layoutscheme") !== undefined)
+                this.options.layoutScheme = obj.data("layoutscheme");
             if (obj.data("total") !== undefined)
                 this.options.total = parseInt(obj.data("total"));
-            if (obj.data("pageindex") !== undefined)
-                this.options.pageIndex = parseInt(obj.data("pageindex"));
             if (obj.data("pagesize") !== undefined)
                 this.options.pageSize = parseInt(obj.data("pagesize"));
             if (obj.data("pagegroupsize") !== undefined)
                 this.options.pageGroupSize = parseInt(obj.data("pagegroupsize"));
+            if (obj.data("pageindex") !== undefined)
+                this.options.pageIndex = parseInt(obj.data("pageindex"));
             if (obj.data("leftformatestring") !== undefined)
                 this.options.leftFormateString = obj.data("leftformatestring");
             if (obj.data("rightformatestring") !== undefined)
@@ -308,15 +397,13 @@ var BootstrapPagination = function (obj, option) {
                 this.options.firstPageText = obj.data("firstpagetext");
             if (obj.data("lastpagetext") !== undefined)
                 this.options.lastPageText = obj.data("lastpagetext");
-
-            //if (obj.data("showgotoinput") !== undefined) {
-            //    var attrShowGotoInput = obj.data("showgotoinput");
-            //    this.options.showGotoInput = (attrShowGotoInput === true || attrShowGotoInput == "true" || attrShowGotoInput == "True");
-            //}
-            if (obj.data("gotoinputplaceholder") !== undefined)
-                this.options.gotoInputPlaceholder = obj.data("gotoinputplaceholder");
-            if (obj.data("entertimeout") !== undefined)
-                this.options.enterTimeout = parseInt(obj.data("entertimeout"));
+            if (obj.data("pageinput-placeholder") !== undefined)
+                this.options.pageInputPlaceholder = obj.data("pageinput-placeholder");
+            if (obj.data("pageinput-timeout") !== undefined)
+                this.options.pageInputTimeout = parseInt(obj.data("pageinput-timeout"));
+            if (obj.data("pagesizelist") !== undefined) {
+                this.options.pageSizeList = eval(obj.data("pagesizelist"));
+            }
             if (obj.data("pagechanged") !== undefined) {
                 var attrPageChanged = obj.data("pagechanged");
                 if (typeof (attrPageChanged) == "function") {
@@ -334,13 +421,16 @@ var BootstrapPagination = function (obj, option) {
                     };
                 }
             }
-            if (obj.data("layoutscheme") !== undefined)
-                this.options.layoutScheme = obj.data("layoutscheme");
             //#endregion
 
             if (option !== undefined) {
                 $.extend(true, this.options, option); //合并由用户代码设置的参数
+                if (option.pageSizeList) {
+                    this.options.pageSizeList = option.pageSizeList;
+                }
             }
+
+            this.fixPageIndex();
         }
 
         this.init();
